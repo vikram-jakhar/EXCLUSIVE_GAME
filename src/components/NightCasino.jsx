95fe5e854866e420a7a1804114546325f82f24dd
vikram-jakhar <vikramjakhar144@gmail.com>
2024-02-05 11:46:06 +0530

    update

--- src/components/NightCasino.jsx
+++ src/components/NightCasino.jsx
@@ -11,7 +11,7 @@ const NightCasino = () => {
                         <h2 className="text-[36px] md:text-[48px] font-normalfont-anton sm:leading-[57px] text-black">
                             Por qué elegirnos
                         </h2>
-                        <p className=" text-base font-medium leading-[25px] font-inter text-[#334349] pt-1 sm:pt-4">
+                        <p className="text-base font-medium leading-[25px] font-inter text-[#334349] pt-1 sm:pt-4">
                             Con Exclusive Games tenés Exclusivos beneficios. Te reintegramos
                             todo lo invertido en fichas en la moneda que elijas. En Exclusive
                             Games siempre sumamos nuevos juegos. Juegos crash, los juegos
